f7dc0845200c0b7ccb1fcf6fc9bedd4d28013e4e
Michelle <mwan391@aucklanduni.ac.nz>
2022-07-09 10:36:56 +1200

    add event schema

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,23 @@ import express from "express"
 import cors from "cors"
 import routes from "./routes/index.js";
 
+import mongoose from "mongoose"
 import Person from "./models/person.js";
+import Event from "./models/events.js"
 
+import dotenv from "dotenv"
+dotenv.config()
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
+
+mongoose.connect(url).then(result => {
+	console.log('connected to MongoDB')
+}).catch((error) => {
+	console.log('error connecting to MongoDB:', error.message)
+})
 
 
 const app = express()
-
 
 app.use(cors())
 app.use(express.json())
@@ -21,6 +32,27 @@ app.get('/api/persons', (request, response) => {
 	Person.find({}).then(persons => {
 		response.json(persons)
 	})
+})
+
+app.get('/api/events', (req, res) => {
+	Event.find({}).then(events => {
+		res.json(events)
+	})
+})
+
+app.post('/api/events', (req, res) => {
+	const body = req.body
+	const event = new Event({
+		name: body.name,
+		date: body.date,
+		people: body.people,
+		description: body.description
+	})
+
+	event.save().then(savedEvent => {
+		response.json(savedEvent)
+	})
+
 })
 
 const PORT = 3001
--- Backend/models/person.js
+++ Backend/models/person.js
@@ -1,14 +1,4 @@
 import mongoose from "mongoose"
-import dotenv from "dotenv"
-dotenv.config()
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-
-mongoose.connect(url).then(result => {
-	console.log('connected to MongoDB')
-}).catch((error) => {
-	console.log('error connecting to MongoDB:', error.message)
-})
 
 const personSchema = new mongoose.Schema({
 	name: String,
