0a1f3d5d8b1b4fecb89d1b3cda55647401d9ce9b
Michelle <mwan391@aucklanduni.ac.nz>
2022-07-08 22:57:29 +1200

    initialise mongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -2,16 +2,25 @@ import express from "express"
 import cors from "cors"
 import routes from "./routes/index.js";
 
+import Person from "./models/person.js";
+
+
 
 const app = express()
 
 
 app.use(cors())
 app.use(express.json())
-app.use("/api", routes);
+// app.use("/api", routes);
 
 app.get('/', (request, response) => {
 	response.send('<h1>Team Beep</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 })
 
 const PORT = 3001
---
+++ Backend/models/person.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
+
+mongoose.connect(url).then(result => {
+	console.log('connected to MongoDB')
+}).catch((error) => {
+	console.log('error connecting to MongoDB:', error.message)
+})
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	username: String,
+	password: String,
+})
+
+personSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+export default mongoose.model("Person", personSchema);
