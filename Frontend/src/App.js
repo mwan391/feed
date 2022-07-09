2bbd3bb5dbaf0fbab383a672d840fb3c9b431f62
Jaeden <jaedenjwest@gmail.com>
2022-07-09 14:33:09 +1200

    add context api

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,41 +4,41 @@ import Home from './pages/home';
 import Navbar from './components/Navbar';
 import { useState } from 'react';
 
+import { UserContext } from './utils/UserContext';
 import personService from './services/persons';
 import loginService from './services/login';
 
 function App() {
-
-  const [user, setUser] = useState(null)
-
+  const [user, setUser] = useState(null);
 
   const handleLogin = async (username, password) => {
-
     try {
       const user = await loginService.login({
-        username, password,
-      })
+        username,
+        password,
+      });
 
-      window.localStorage.setItem(
-        'loggedBeepUser', JSON.stringify(user)
-      )
-      personService.setToken(user.token)
-      setUser(user)
+      window.localStorage.setItem('loggedBeepUser', JSON.stringify(user));
+      personService.setToken(user.token);
+      setUser(user);
     } catch (exception) {
-      console.log('Wrong username or password')
+      console.log('Wrong username or password');
     }
-  }
-
-
+  };
 
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn handleSumbit={handleLogin} />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <UserContext.Provider value={{ user }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/signin"
+            element={<SignIn handleSumbit={handleLogin} />}
+          />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </UserContext.Provider>
     </div>
   );
 }
