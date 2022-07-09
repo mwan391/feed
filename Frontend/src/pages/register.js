293596d878ebc593113f9860a255b14d35811eec
Pinghang Fan <pinghangfan1022@gmail.com>
2022-07-09 18:43:50 +1200

    added register page componment

---
+++ Frontend/src/pages/register.js
@@ -0,0 +1,144 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { useState } from 'react'
+
+
+const theme = createTheme();
+
+export default function Register(props) {
+  const navigate = useNavigate();
+  const submitLogin = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    var axios = require('axios');
+    var fetchData = JSON.stringify({
+      "username": data.get('email'),
+      "name": data.get("name"),
+      "password": data.get('password')
+    });
+
+    var config = {
+      method: 'post',
+      url: 'http://localhost:3001/api/persons',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : fetchData
+    };
+
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      setUsername('')
+      setName('')
+      setPassword('')
+      navigate("/")
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+
+  const [username, setUsername] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <Box component="form" onSubmit={submitLogin} noValidate sx={{ mt: 1 }}>
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              autoFocus
+              value={name}
+              type="text"
+              onChange={({ target }) => setName(target.value)}
+
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={username}
+              type="text"
+              onChange={({ target }) => setUsername(target.value)}
+
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Register
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
