664b02887996933686f5a3dd9dabfa2a3455983f
Jaeden <jaedenjwest@gmail.com>
2022-07-09 12:25:01 +1200

    Add logo to Navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,18 @@
-
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import SignIn from './pages/signIn';
 import Home from './pages/home';
 import Navbar from './components/Navbar';
 
 function App() {
-	return (
-	  <div className="App">
-    <Navbar />
-		<Routes>
-			<Route path="/" element={<Home />} />
-		  	<Route path="/signin" element={<SignIn />} />
-		</Routes>
-	  </div>
-	);
-  }
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
