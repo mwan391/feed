b663f5e954056411227c26031327c394d728f7ba
Jaeden <jaedenjwest@gmail.com>
2022-07-09 15:21:09 +1200

    make logo clickable

--- Frontend/src/Components/CardGrid.js
+++ Frontend/src/Components/CardGrid.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MediaCard from './Card';
 function CardGrid() {
   return (
-    <div class="container mx-auto px-12 mt-[6rem]">
+    <div class="container mx-auto px-12 mt-8">
       <div class="flex flex-wrap -mx-1 lg:-mx-4 justify-between px-20">
         <MediaCard />
         <MediaCard />
