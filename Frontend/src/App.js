bd44079606d668923bfd4e16e5b8577875d4bba8
Jaeden <jaedenjwest@gmail.com>
2022-07-09 13:52:47 +1200

    add home route

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
     </div>
